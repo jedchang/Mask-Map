6487d8e920cbb8a409d6a14c62e911ef46b5dbfd
jedchang <march.24@gmail.com>
2024-11-19 10:35:04 +0800

    更新內容

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ cd dist
 git init 
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/jedchang/TheF2E-Vue-MaskMap.git master:gh-pages
+git push -f https://github.com/jedchang/Mask-Map.git master:gh-pages
 cd -
